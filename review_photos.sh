b3e638f6ac16a8100edcf92c3d7588e6d00229e2
Vesper <veswill3@gmail.com>
2017-01-04 23:30:50 +0700

    initial commit

---
+++ review_photos.sh
@@ -0,0 +1,21 @@
+keep="mv %F backup/"
+delete="mv %F to_delete/"
+add_to_pano_grp="echo mv %F to_pano/XXX/ >> pano_grps.txt"
+start_pano_grp="echo 'mkdir to_pano/XXX\nmv %F to_pano/XXX/' >> pano_grps.txt"
+
+while true; do
+    read -p "What do you want to review? [R]eview, [U]pload, or [O]ther to specify: " choice
+    case $choice in
+        [Uu]* ) DIRECTORY="to_upload"; break;;
+        [Rr]* ) DIRECTORY="to_review"; break;;
+        [Oo]* ) read -p "Directory name: " DIRECTORY; break;;
+        * ) echo "Please answer [R]eview or [U]pload.";;
+    esac
+done
+
+feh -Fd --draw-tinted \
+--action1 "$add_to_pano_grp" \
+--action2 "$start_pano_grp" \
+--action3 "$delete" \
+--action4 "$keep" \
+--draw-actions "$DIRECTORY"
